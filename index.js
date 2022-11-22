2bd862b665e35baaa2a0dd39b728e6257ba1d436
icika-max <icika-max>
2022-11-22 14:36:03 +0300

    completed assignent

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 2
+let num2 = 31
+const multiply = (num1 * num2)
+
+const random= Math.floor(Math.random() * 10);
+ let num3 =24
+ let num4 = 5
+
+const mod =(num3%num4);
+const max = 20
+console.log(Math.max())
+
+
+
+
+
 
 // Write your code here
